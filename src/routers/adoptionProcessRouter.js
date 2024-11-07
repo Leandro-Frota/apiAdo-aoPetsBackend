1e636be2e30bc6213fa61da20c740ebab01f2ab1
Leandro <leandromfrota@gmail.com>
2024-11-07 15:08:35 -0300

    included function that changes the status of the pet entity and altere tipo ohf the dada_adocao

--- src/routers/adoptionProcessRouter.js
+++ src/routers/adoptionProcessRouter.js
@@ -4,7 +4,11 @@ import { AdoptionProcessController } from "../controllers/adoptionProcessControl
 const adoptionProcessRouter = Router()
 const adoptionProcessController = new AdoptionProcessController()
 
-
+adoptionProcessRouter.get('/',adoptionProcessController.getProcessAdopt)
+adoptionProcessRouter.post('/',adoptionProcessController.registerAdoptionProcess)
+adoptionProcessRouter.get('/:id',adoptionProcessController.getProcessAdoptById)
+adoptionProcessRouter.put('/:id',adoptionProcessController.updateAdoptionProcess)
+adoptionProcessRouter.delete('/:id',adoptionProcessController.deleteAdoptionProcess)
 
 
 export{adoptionProcessRouter}
